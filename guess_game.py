e251efd159fb764876c56be0123a8430e341405c
Graciela Cagua <grachycagua@gmail.com>
2025-06-11 07:27:28 -0500

    j

--- guess_game.py
+++ guess_game.py
@@ -38,6 +38,10 @@ def leaderboard():
                     .execute())
     return render_template("leaderboard.html", players=res.data)
 
+@app.route("/health")
+def health():
+    return "OK", 200
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
 
